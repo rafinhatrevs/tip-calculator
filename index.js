6ab09db8717b1e9783e851a7518a9f63f6f8afe1
Rafaella Trevizan <rafaella_trevizan@hotmail.com>
2024-05-13 21:28:29 -0300

    Pequenos ajustes

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function formatMoney(value) {
-    value = Math.ceil(value * 100) / 100; //arredonda o valor para mais
-    value = value.toFixed(2); //fixa o valor decimal em 2 casas
+    value = Math.ceil(value * 100) / 100; // arredonda o valor para mais
+    value = value.toFixed(2); // fixa o valor decimal em 2 casas
     return '$ ' + value;
 }
 
-function formatSplit(value) { //retorna person quando 1 pessoa e people quando mais pessoas
+function formatSplit(value) { // retorna person quando 1 pessoa e people quando mais pessoas
     if (value == 1) {
         return value + ' person';
     }
@@ -13,17 +13,17 @@ function formatSplit(value) { //retorna person quando 1 pessoa e people quando m
 }
 
 function update() {
-    let bill = Number(document.getElementById('your_bill').value);
-    let tip_percent = document.getElementById('tip_input').value;
-    let split = document.getElementById('split_input').value;
+    let bill = Number(document.getElementById('your-bill').value);
+    let tip_percent = document.getElementById('tip-input').value;
+    let split = document.getElementById('split-input').value;
 
     let tip_value = bill * (tip_percent / 100);
     let bill_total = bill + tip_value;
     let bill_each = bill_total / split;
 
-    document.getElementById('tip_percent').innerHTML = tip_percent + ' %';
-    document.getElementById('tip_value').innerHTML = formatMoney(tip_value);
-    document.getElementById('total_with_tip').innerHTML = formatMoney(bill_total);
-    document.getElementById('split_value').innerHTML = formatSplit(split);
-    document.getElementById('bill_each').innerHTML = formatMoney(bill_each);
+    document.getElementById('tip-percent').innerHTML = tip_percent + ' %';
+    document.getElementById('tip-value').innerHTML = formatMoney(tip_value);
+    document.getElementById('total-with-tip').innerHTML = formatMoney(bill_total);
+    document.getElementById('split-value').innerHTML = formatSplit(split);
+    document.getElementById('bill-each').innerHTML = formatMoney(bill_each);
 }
